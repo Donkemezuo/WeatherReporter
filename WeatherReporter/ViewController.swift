d391cc0a1df73988102e0041e867948882fbe88a
Tariladou Donkemezuo <raymonddonkemezuojnr@gmail.com>
2023-09-05 16:00:26 -0500

    First commit

---
+++ WeatherReporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherReporter
+//
+//  Created by Raymond Donkemezuo on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
